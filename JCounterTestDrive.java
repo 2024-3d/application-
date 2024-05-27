1611c4db7339677edf52ac03e53438121eb69629
kure728 <liwuwu72@gmail.com>
2024-05-27 17:15:48 +0900

    Create JCounterTestDrive.java

---
+++ JCounterTestDrive.java
@@ -0,0 +1,11 @@
+package pp1.lesson03.sample.testdrive;
+
+import pp1.lesson03.sample.counter.JCounterView;
+
+public class JCounterTestDrive {
+
+	public static void main(String[] args) {
+		JCounterView view = new JCounterView();
+		view.createView();
+	}
+}
