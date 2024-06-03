ca43308f18977cbb555838374376a810e6ae857d
kure728 <liwuwu72@gmail.com>
2024-06-03 17:36:52 +0900

    Create Calendar.java

---
+++ home/ver2.2/Calendar.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Calendar extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("カレンダー画面"));
+        return panel;
+    }
+}
