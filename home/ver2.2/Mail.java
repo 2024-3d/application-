ea9ef4bd289b86146656b267aee3797dc1f34557
kure728 <liwuwu72@gmail.com>
2024-06-03 17:36:24 +0900

    Create Mail.java

---
+++ home/ver2.2/Mail.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Mail extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("メール画面"));
+        return panel;
+    }
+}
