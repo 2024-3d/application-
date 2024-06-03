c60d85c6420c79b3a3c73e23147603c4feba5457
kure728 <liwuwu72@gmail.com>
2024-06-03 17:00:49 +0900

    Create Home.java

---
+++ home/ver2.0/Home.java
@@ -0,0 +1,23 @@
+import java.awt.Panel;
+import java.awt.Label;
+import java.awt.GridLayout;
+public class Home implements Menu{
+    Panel panel;
+    Label label1;
+    Label label2;
+    String name;
+    public Home(){
+        name = "ホーム";
+    }
+    public String getName(){
+        return name;
+    }
+    public Panel panel(){
+        panel = new Panel(new GridLayout(2, 1));
+        label1 = new Label("期限が近い課題");
+        label2 = new Label("新しい連絡 : 3件");
+        panel.add(label1);
+        panel.add(label2);
+        return panel;
+    }
+}
