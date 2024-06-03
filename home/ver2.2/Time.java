c390e8f25bc2a8b3ed05b88598be46aae8e14f1c
kure728 <liwuwu72@gmail.com>
2024-06-03 17:36:00 +0900

    Create Time.java

---
+++ home/ver2.2/Time.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Time extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("時間割画面"));
+        return panel;
+    }
+}
