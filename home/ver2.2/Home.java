79fc1562562c48eb5b4e0b4a907fe55ccf0d480f
kure728 <liwuwu72@gmail.com>
2024-06-03 17:35:45 +0900

    Create Home.java

---
+++ home/ver2.2/Home.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Home extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("ホーム画面"));
+        return panel;
+    }
+}
