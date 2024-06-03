aa2ab39f8228f5f293874c52c6363864aa281266
kure728 <liwuwu72@gmail.com>
2024-06-03 17:37:30 +0900

    Create Question.java

---
+++ home/ver2.2/Question.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Question extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("質問画面"));
+        return panel;
+    }
+}
