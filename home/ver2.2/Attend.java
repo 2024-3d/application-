d3527792b4367237c4deee6197e78e3d9ff3469e
kure728 <liwuwu72@gmail.com>
2024-06-03 17:37:10 +0900

    Create Attend.java

---
+++ home/ver2.2/Attend.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Attend extends Menu {
+    public Panel panel() {
+        Panel panel = new Panel();
+        panel.add(new Label("出欠画面"));
+        return panel;
+    }
+}
