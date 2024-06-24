9e092e7a13be0162855ae787242d7a2f52cb4e96
kure728 <liwuwu72@gmail.com>
2024-06-24 17:54:45 +0900

    Create Message.java

---
+++ home/ver3.0/Message.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Message extends AppFunction {
+    @Override
+    public Panel panel() {
+        Panel panel = new Panel();
+        // 機能は後で追加予定
+        return panel;
+    }
+}
